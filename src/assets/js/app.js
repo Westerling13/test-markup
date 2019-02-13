4fed3ccb0d692a2a5ca246b23420eec0eb58057e
Alexander Munz <a_munz@list.ru>
2019-02-13 15:38:26 +0300

    Настраивает валидацию форм через jquery-validation

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'what-input';
 import 'slick-carousel';
 import 'owl.carousel';
+import 'jquery-validation';
 
 // Foundation JS relies on a global varaible. In ES6, all imports are hoisted
 // to the top of the file so if we used`import` to import Foundation,
@@ -28,6 +29,53 @@ $('.owl-carousel').owlCarousel({
       items: 3
     }
   }
+});
+
+$('#form').validate({
+  rules: {
+    name: {
+      minlength: 2
+    },
+    phone: {
+      required: true,
+      minlength: 11,
+      maxlength: 11,
+      digits: true
+    },
+    email: {
+      required: true,
+      email: true
+    },
+    agreement: {
+      required: true
+    }
+  },
+  messages: {
+    name: {
+      minlength: 'Введите не менее 2-х символов'
+    },
+    phone: {
+      required: 'Это поле обязательно для заполнения',
+      minlength: 'Введите номер в 11-значном формате',
+      maxlength: 'Введите номер в 11-значном формате',
+      digits: 'Номер должен состоять из цифр'
+    },
+    email: {
+      required: 'Это поле обязательно для заполнения',
+      email: 'Проверьте введенный адрес'
+    },
+    agreement: {
+      required: 'Это поле обязательно для заполнения'
+    }
+  },
+  errorPlacement: function(error, element) {
+      var placement = $(element).data('error');
+      if (placement) {
+        $(placement).append(error)
+      } else {
+        error.insertAfter(element);
+      }
+    }
 });
 
 $(document).foundation();
